820e78612195429778875f384aa915a7ee502a1d
Gokul Kannan <132426290+Gokul5001@users.noreply.github.com>
2023-11-15 22:14:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import './App.css'
+import React, { useState, useEffect,useRef } from 'react'
+import Resume from './components/Resume'
+import NonExistentPage from './components/NonExistentPage'
+import PromptUser from './components/PromptUser'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import {useNavigate,Link} from 'react-router-dom'
+import { HiArrowRight } from 'react-icons/hi'
+
+const converter = require('number-to-words')
+
+function App() {
+  const [userInfo, setUserInfo] = useState({
+  });
+  const [source, setSource] = useState('');
+  const random_number = Math.floor(Math.random()* 10e4)
+  const random_alpha_num = converter.toWords(random_number)
+  const getRandomNumber = (randNum) => {
+    return converter.towards(randNum)
+  }
+  const generateImage = () => {
+    setSource(`https://robohash.org/stefan-${random_alpha_num}`)
+    console.log(source);
+  }
+
+  useEffect(() => {
+    generateImage();
+  }, []);
+  function handleChange(e) {
+    setUserInfo(prevInfo => {
+      return {
+        ...prevInfo,
+        [e.target.name]: e.target.value,
+      };
+    });
+  }
+
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<PromptUser userObject={userInfo} imageSource={source} handleChange={handleChange} />}/>
+          <Route path="/resume" element={<Resume userObject={userInfo} imageSource={source} />} />
+          <Route path="*" element={<NonExistentPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NonExistentPage.jsx
@@ -0,0 +1,13 @@
+// NonExistencePage.js
+import React from 'react'
+
+const NonExistencePage = () => {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+}
+
+export default NonExistencePage
